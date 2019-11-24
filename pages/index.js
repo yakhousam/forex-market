9efbc0ee3e9fe4bc5cf7091212041af35a5de158
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-11-24 17:11:26 +0100

    tested key api with nomics.com example and it works perfectly

--- pages/index.js
+++ pages/index.js
@@ -1,88 +1,36 @@
-import React from 'react'
-import Head from 'next/head'
-import Nav from '../components/nav'
+import React from "react";
+import fetch from "isomorphic-unfetch";
 
-const Home = () => (
-  <div>
-    <Head>
-      <title>Home</title>
-      <link rel="icon" href="/favicon.ico" />
-    </Head>
-
-    <Nav />
-
-    <div className="hero">
-      <h1 className="title">Welcome to Next.js!</h1>
-      <p className="description">
-        To get started, edit <code>pages/index.js</code> and save to reload.
-      </p>
-
-      <div className="row">
-        <a href="https://nextjs.org/docs" className="card">
-          <h3>Documentation &rarr;</h3>
-          <p>Learn more about Next.js in the documentation.</p>
-        </a>
-        <a href="https://nextjs.org/learn" className="card">
-          <h3>Next.js Learn &rarr;</h3>
-          <p>Learn about Next.js by following an interactive tutorial!</p>
-        </a>
-        <a
-          href="https://github.com/zeit/next.js/tree/master/examples"
-          className="card"
-        >
-          <h3>Examples &rarr;</h3>
-          <p>Find other example boilerplates on the Next.js GitHub.</p>
-        </a>
-      </div>
-    </div>
-
-    <style jsx>{`
-      .hero {
-        width: 100%;
-        color: #333;
-      }
-      .title {
-        margin: 0;
-        width: 100%;
-        padding-top: 80px;
-        line-height: 1.15;
-        font-size: 48px;
-      }
-      .title,
-      .description {
-        text-align: center;
-      }
-      .row {
-        max-width: 880px;
-        margin: 80px auto 40px;
-        display: flex;
-        flex-direction: row;
-        justify-content: space-around;
-      }
-      .card {
-        padding: 18px 18px 24px;
-        width: 220px;
-        text-align: left;
-        text-decoration: none;
-        color: #434343;
-        border: 1px solid #9b9b9b;
-      }
-      .card:hover {
-        border-color: #067df7;
-      }
-      .card h3 {
-        margin: 0;
-        color: #067df7;
-        font-size: 18px;
-      }
-      .card p {
-        margin: 0;
-        padding: 12px 0 0;
-        font-size: 13px;
-        color: #333;
-      }
-    `}</style>
+function Page({ prices = [] }) {
+  return <div>
+    <h1>Cryptoasset Prices</h1>
+        <p>
+          This is a demo of the <a href="https://docs.nomics.com">Nomics Cryptoasset API</a>.
+        </p>
+        <table>
+          <thead>
+            <tr>
+              <td>Symbol</td>
+              <td>Price</td>
+            </tr>
+          </thead>
+          <tbody>
+            {prices.map((p) =>
+              <tr key={p.currency}>
+                <td>{p.currency}</td>
+                <td>{p.price}</td>
+              </tr>
+            )}
+          </tbody>
+        </table>
   </div>
-)
+}
 
-export default Home
+Page.getInitialProps = async ({ req }) => {
+  const res = await fetch('https://api.nomics.com/v1/prices?key=4d2553a2b5a8f7c320ea1593e5d7ec92')
+  console.log('res =', res.status)
+  const data = await res.json()
+  return { prices: data }
+}
+
+export default Page
