d044bb17082d1964ae4a83395719914d4c7f18c6
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-11-24 17:48:04 +0100

    exported all files in container folder with index.js

---
+++ containers/index.js
@@ -0,0 +1,4 @@
+export { default as Header } from "./Header";
+export { default as Sidebare } from "./Sidebare";
+export { default as ChartContainer } from "./ChartContainer";
+export { default as Footer } from "./Footer";
