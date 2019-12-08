31684be7ba4b704dd37b05e6f692a480c3a9e4be
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-12-08 16:49:36 +0100

    added logo currency

--- components/PriceRow.js
+++ components/PriceRow.js
@@ -1,22 +1,32 @@
 import React from 'react'
 
-const PriceRow = ({currency, price}) => {
+const PriceRow = ({name, price, logo_url, ...rest}) => {
+  console.log('logo =', logo_url)
+  console.log(rest)
   return (
     <div className='price-row'>
-      <div className='currency'>{currency}</div>
+      <img src={logo_url} />
+      <div className='name'>{name}</div>
       <div className='price'>{price}</div>
       <style jsx>{`
         .price-row{
           display: flex;
           border: thin solid black;
+          align-items: center;
+          padding: 5px;
         }
         .price{
           flex: 1;
           text-align: right;
         }
-        .currency{
+        .name{
           flex: 1;
           
+        }
+        img{
+          height: 40px;
+          width: 40px;
+          margin-right: 5px;
         }
       `}</style>
     </div>
