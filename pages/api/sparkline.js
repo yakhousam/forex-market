b64e5907a4e04674288e0788d887b0c50816cd1e
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-12-17 19:09:31 +0100

    used binance api

--- pages/api/sparkline.js
+++ pages/api/sparkline.js
@@ -4,16 +4,19 @@ import fetch from "isomorphic-unfetch";
 export default async (req, res) => {
   const key = "4d2553a2b5a8f7c320ea1593e5d7ec92&ids";
   const start = "2019-01-01T00:00:00Z";
+  const url = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h'
+  console.log(url)
   try {
-    const response = await fetch(
-      `https://api.nomics.com/v1/currencies/sparkline?key=${key}&start=${start}`
-    );
-    if (response.status === 200) {
+    // const response = await fetch(
+    //   `https://api.nomics.com/v1/currencies/sparkline?key=${key}&start=${start}`
+    // );
+    const response = await fetch(url)
+    // if (response.status === 200) {
       const json = await response.json();
-      // console.log("json =", json);
+      console.log("json =", json);
      
       res.status(200).json(json);
-    }
+    // }
   } catch (error) {
     console.error(error);
     res.status(500).end();
