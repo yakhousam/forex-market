18ddaad0c5d19ec93bdab285f442978b313cb529
yakhousam <42283612+yakhousam@users.noreply.github.com>
2019-12-18 16:23:44 +0100

    ref limit 24 candle

--- pages/api/sparkline.js
+++ pages/api/sparkline.js
@@ -4,19 +4,21 @@ import fetch from "isomorphic-unfetch";
 export default async (req, res) => {
   const key = "4d2553a2b5a8f7c320ea1593e5d7ec92&ids";
   const start = "2019-01-01T00:00:00Z";
-  const url = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h'
+  const url = 'https://api.binance.com/api/v3/klines?symbol=BTCUSDT&interval=1h&limit=24'
   console.log(url)
   try {
     // const response = await fetch(
     //   `https://api.nomics.com/v1/currencies/sparkline?key=${key}&start=${start}`
     // );
     const response = await fetch(url)
-    // if (response.status === 200) {
+    if (response.status === 200) {
       const json = await response.json();
       console.log("json =", json);
      
       res.status(200).json(json);
-    // }
+    }else{
+      res.status(response.status).end()
+    }
   } catch (error) {
     console.error(error);
     res.status(500).end();
